89e2841fc6158717f0bcbd83953fda0612c1e6af
Jim <jim_f_d@hotmail.com>
2023-02-20 15:26:01 +0100

    api connection + html generating

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const url = 'https://quote.api.fdnd.nl/v1/quote'
+fetch('https://quote.api.fdnd.nl/v1/quote')
+  .then(response => response.json())
+  .then(data => {
+ 
+  const genCardContainer = document.createElement('article');
+  genCardContainer.setAttribute('id', 'card-container');
 
-const data = fetch(url)
+  const genCardHeading = document.createElement('h1');
+  genCardHeading.setAttribute('id', 'card-heading')
+  genCardContainer.appendChild(genCardHeading);
 
-    .then(response => response.json())
-    .then(data => console.log(data))
+  const genCardText = document.createElement('p');
+  genCardText.setAttribute('id', 'card-text')
+  genCardContainer.appendChild(genCardText);
+
+  document.body.appendChild(genCardContainer);
+
+  const arrayNum = Math.floor(Math.random() * 15);
+  const cardContainer = document.getElementById('card-container');
+  const cardHeading = document.getElementById('card-heading');
+  const cardText = document.getElementById('card-text');
+
+  cardHeading.innerHTML = data.data[arrayNum].name;
+  cardText.innerHTML = data.data[arrayNum].text || data.data[arrayNum].bio;
+})
