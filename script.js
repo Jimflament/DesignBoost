de6d709a3070266faf8572994d90a6820cf631c5
Jim <jim_f_d@hotmail.com>
2023-02-21 09:14:28 +0100

    added comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// API Connection
 fetch('https://quote.api.fdnd.nl/v1/quote')
   .then(response => response.json())
   .then(data => {
  
+// Card generating and placing
   const genCardContainer = document.createElement('article');
   genCardContainer.setAttribute('id', 'card-container');
 
@@ -15,11 +17,13 @@ fetch('https://quote.api.fdnd.nl/v1/quote')
 
   document.body.appendChild(genCardContainer);
 
+//   HTML Variables
   const arrayNum = Math.floor(Math.random() * 15);
   const cardContainer = document.getElementById('card-container');
   const cardHeading = document.getElementById('card-heading');
   const cardText = document.getElementById('card-text');
 
+//   HTML Content
   cardHeading.innerHTML = data.data[arrayNum].name;
-  cardText.innerHTML = data.data[arrayNum].text || data.data[arrayNum].bio;
+  cardText.innerHTML = data.data[arrayNum].text
 })
